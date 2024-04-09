858bbeffdc0bdd6d94aa915f50dc64107f481091
Isha Das <ishantkdas@gmail.com>
2024-04-09 14:55:15 +0530

    All codes corrected & working fine

--- bubble_sort_prctc.py
+++ bubble_sort_prctc.py
@@ -1,11 +1,11 @@
-#Making  an array
-arr  = [2, 5, 1, 2, 3]
+arr = [2, 5, 1, 2, 3]
+
 def bubble_sort(arr):
     n = len(arr)
     for i in range(n):
         for j in range(0, n-i-1):
-            if arr[i] < arr[j]:
-                arr[i], arr[j] = arr[j], arr[i]
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
     return arr
-print(bubble_sort(arr))
 
+print(bubble_sort(arr))
